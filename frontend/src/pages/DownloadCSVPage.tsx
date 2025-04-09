3f24e2cdda5008a36ed5e052a0134f9e1d5cf7df
TheNaomiii <naomiipham@hotmail.com>
2025-04-09 21:06:42 +1000

    Download CSV button now works and gives correct CSV format

--- frontend/src/pages/DownloadCSVPage.tsx
+++ frontend/src/pages/DownloadCSVPage.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { downloadCSV } from '../utils/API';
 
 const DownloadCSVPage: React.FC = () => {
+  const handleDownload = async () => {
+    await downloadCSV();
+  };
+
   return (
     <div>
       <h1>Download CSV Page</h1>
+      <button onClick={handleDownload} className="upload-button">
+        Download CSV
+      </button>
     </div>
   );
 };
