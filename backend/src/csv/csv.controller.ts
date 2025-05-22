1b910cbc84cff54e90e433623c384a9edb64d0c7
TheNaomiii <naomiipham@hotmail.com>
2025-05-22 23:04:37 +1000

    Updated swagger documentation

--- backend/src/csv/csv.controller.ts
+++ backend/src/csv/csv.controller.ts
@@ -1,18 +1,30 @@
 import { Controller, Get, Req, Res } from '@nestjs/common';
 import { CsvService } from './csv.service';
 import { Request, Response } from 'express';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiConsumes,
+  ApiBody,
+  ApiParam,
+  ApiResponse,
+} from '@nestjs/swagger';
 
 @Controller('api/csv')
 export class CsvController {
   constructor(private readonly csvService: CsvService) { }
 
   @Get('preview')
+  @ApiOperation({ summary: 'Generate the CSV format using the files uploaded in this session' })
+  @ApiResponse({ status: 200, description: 'The CSV format has been generated successfully.' })
   async previewCsv(@Req() req: Request): Promise<string> {
     const sessionToken = req.cookies['session_token'];
     return this.csvService.generateCsv(sessionToken);
   }
 
   @Get('generate')
+  @ApiOperation({ summary: 'Generate the CSV format using the files uploaded in this session, and prompt the browser to download it' })
+  @ApiResponse({ status: 200, description: 'The CSV format has been generated successfully and the browser was prompted to download.' })
   async generateCsvDownload(@Req() req: Request, @Res() res: Response) {
     try {
       const sessionToken = req.cookies['session_token'];
